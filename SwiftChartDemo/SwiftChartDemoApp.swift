ba8aee0eae60c9b4a4f7dbfd52d68a01f6fd3113
itsuki <itsuki.enjyoy@gmail.com>
2025-02-22 09:31:52 +0900

    Initial Commit

---
+++ SwiftChartDemo/SwiftChartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartDemoApp.swift
+//  SwiftChartDemo
+//
+//  Created by Itsuki on 2025/02/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
