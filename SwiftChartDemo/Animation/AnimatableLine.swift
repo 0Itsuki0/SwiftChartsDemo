a65d40fed9f990c217c805f43adf5ac96cc5cbd7
itsuki <itsuki.enjyoy@gmail.com>
2025-04-14 06:04:33 +0900

    update line animation

--- SwiftChartDemo/Animation/AnimatableLine.swift
+++ SwiftChartDemo/Animation/AnimatableLine.swift
@@ -32,6 +32,20 @@ extension LineMarkData {
 }
 
 struct LineMarkAnimationDemo: View {
+    var body: some View {
+        VStack(spacing: 64) {
+            ColoredChartBackground()
+            TransparentChartBackground()
+        }
+        .padding(.top, 32)
+        .padding(.horizontal, 48)
+        .frame(maxHeight: .infinity, alignment: .top)
+        .background(.gray.opacity(0.1))
+    }
+}
+
+
+private struct ColoredChartBackground: View {
     
     @State private var dataList = LineMarkData.sampleData
  
@@ -51,6 +65,8 @@ struct LineMarkAnimationDemo: View {
     var body: some View {
         
         VStack(spacing: 24) {
+            Text("With Colored Background")
+                .fontWeight(.bold)
 
             Toggle(isOn: $showData, label: {
                 Text("Show Data")
@@ -77,10 +93,6 @@ struct LineMarkAnimationDemo: View {
             })
 
         }
-        .padding(.top, 32)
-        .padding(.horizontal, 48)
-        .frame(maxHeight: .infinity, alignment: .top)
-        .background(.gray.opacity(0.1))
         .onChange(of: showData, {
             if showData {
                 withAnimation(.linear(duration: 2.0)) {
@@ -98,7 +110,7 @@ struct LineMarkAnimationDemo: View {
         Chart {
             v()
         }
-        .frame(height: 200)
+        .frame(height: 180)
         .chartXScale(domain: minDate...maxDate)
         .chartYScale(domain: 0...6000)
         .chartYAxis {
@@ -111,6 +123,94 @@ struct LineMarkAnimationDemo: View {
             Color.white
                 .frame(width: proxy.plotSize.width + 80, height: proxy.plotSize.height + 80)
         })
+    }
+}
+
+
+struct TransparentChartBackground: View {
+    
+    @State private var dataList = LineMarkData.sampleData
+ 
+    @State private var showData = false
+    @State private var endFraction: CGFloat = 0.0
+    
+    private let minDate = LineMarkData.sampleData.min(by: { first, second in
+        first.date < second.date
+    })?.date ?? Date()
+    
+    private let maxDate = LineMarkData.sampleData.max(by: { first, second in
+        first.date < second.date
+    })?.date ?? Date()
+    
+    
+    @State private var plotSize: CGSize = .zero
+
+    
+    var body: some View {
+        
+        VStack(spacing: 24) {
+            Text("With Transparent Background")
+                .fontWeight(.bold)
+
+
+            Toggle(isOn: $showData, label: {
+                Text("Show Data")
+            })
+            .tint(.blue)
+            .padding(.bottom, 16)
+                
+
+            self.chart(v: {
+                ForEach(dataList) { data in
+                    LineMark(x: .value("", data.date), y: .value("", data.value))
+                        .interpolationMethod(.catmullRom)
+                }
+            })
+            .overlay(content: {
+                GeometryReader { proxy in
+                    DispatchQueue.main.async {
+                        self.plotSize = proxy.size
+                    }
+                    return Color.clear
+                }
+            })
+            .mask(
+                Rectangle()
+                    .fill(Color.white)
+                    .padding(.trailing, (1-endFraction) * self.plotSize.width)
+            )
+            // to show grid lines
+            .chartOverlay(alignment: .center, content: { _ in
+                self.chart(v: {})                
+            })
+
+        }
+        .onChange(of: showData, {
+            if showData {
+                withAnimation(.linear(duration: 2.0)) {
+                    endFraction = 1.0
+                }
+            } else {
+                endFraction = 0.0
+            }
+        })
+    }
+
+    
+    private func chart<V: ChartContent>(@ChartContentBuilder v: () -> V) -> some View {
+        Chart {
+            v()
+        }
+        .frame(height: 180)
+        .chartXScale(domain: minDate...maxDate)
+        .chartYScale(domain: 0...6000)
+        .chartYAxis {
+            AxisMarks(preset: .aligned) { _ in
+                AxisGridLine()
+                AxisValueLabel(horizontalSpacing: 16)
+            }
+        }
+
     }
 }
 
